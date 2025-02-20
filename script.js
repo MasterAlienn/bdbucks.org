bdc105fcfe84f960b6445dc79815be1b3c9dc8f1
MasterAlienn <ryannforme@gmail.com>
2025-02-19 23:19:00 -0500

    Refactor account management: remove unused CSS, update HTML to use one stylesheet, and enhance JSON structure for user data

--- script.js
+++ script.js
@@ -46,33 +46,13 @@ function logIn() {
     let found;
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    fetch("../tacc.json").then(response => {
-        if (!response.ok) {
-            throw new Error("Can't get file");
+    let pac = {
+        operation: "login",
+        data: {
+            username: username,
+            password: password
         }
-        return response.json();
-    }).then(data => {
-        acc = data;
-    }).catch(error => {
-        console.error("There was something wrong with the fetch operation:", error);
-    });
-    // make it wait for fetch to finish
-    const userNum = acc.accounts.length;
-    for (i = 0; i > userNum; i++) {
-        let un = acc.accounts[i].username;
-        found = false;
-        if (username == un) {
-            id = acc.accounts[i].id - 1;
-            found = true;
-            break;
-        }
-    }
-    if (!found) {
-        console.error("User doesn't exist");
-    }
-    if (found && password == acc.accounts[id].password) {
-        setCookie("username", username);
-    }
+    };
 }
 function signUp() {
     const email = document.getElementById("email").value;
@@ -80,25 +60,15 @@ function signUp() {
     const password = document.getElementById("password").value;
     const cpassword = document.getElementById("confirm").value;
     const domain = email.split("@")[1];
-    const isAllowed = domain == "rocklandschools.org";
-    if (!isAllowed) {
-        document.getElementById("emaildiv").innerHTML = "Invalid email";
-    } else {
-        document.getElementById("emaildiv").innerText = "\n";
-    }
     const areSame = password == cpassword;
-    if (isAllowed && areSame) {
-        let pac = {
-            operation: "mkacc",
-            data: {
-                acctype: "pending",
-                email: email,
-                username: username,
-                password: password
-            }
-        };
-        
-    }    
+    let pac = {
+        operation: "mkacc",
+        data: {
+            email: email,
+            username: username,
+            password: password
+        }
+    };
 }
 function getUsername() {
     return document.cookie.replace("username=", "");
